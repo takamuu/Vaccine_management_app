33ee998a85df16e147b60fb21b9430f19cf9bbc6
takashi <mr.takamuu@gmail.com>
2021-03-12 10:48:00 +0900

    factory_bot_rails environment construction

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,8 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  # 使用する際に FactoryBot を省略できるようにする
+  config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
